a35e51c6ccefd282d240790d5e508285c463677b
Rekha Singh <99745970+rekhafauzdar@users.noreply.github.com>
2025-03-04 09:09:38 +0530

    Admin Work

--- langScript.js
+++ langScript.js
@@ -1,12 +1,30 @@
+// function changeLanguage(language) {
+//     fetch(`lang/${language}.json`) // Fetch the JSON file
+//       .then(response => response.json())
+//       .then(data => {
+//         // Replace the content dynamically
+//         document.getElementById("title").innerText = data.title;
+//         document.getElementById("description").innerText = data.description;
+//         document.getElementById("contact").innerText = data.contact;
+//       })
+//       .catch(error => console.error("Error loading language file:", error));
+//   }
+  
+
+
+
 function changeLanguage(language) {
-    fetch(`lang/${language}.json`) // Fetch the JSON file
+    // fetch(`lang/${language}.json`)
+    fetch(`lang/${language}.json?${new Date().getTime()}`)
       .then(response => response.json())
       .then(data => {
-        // Replace the content dynamically
-        document.getElementById("title").innerText = data.title;
-        document.getElementById("description").innerText = data.description;
-        document.getElementById("contact").innerText = data.contact;
+        document.querySelectorAll("[data-key]").forEach(element => {
+          let key = element.getAttribute("data-key");
+          if (data[key]) {
+            element.innerText = data[key];
+          }
+        });
       })
-      .catch(error => console.error("Error loading language file:", error));
+      .catch(error => console.error("Language file not loaded", error));
   }
   
